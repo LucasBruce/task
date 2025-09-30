738e22cc47c01443f486b3635814dfabdd2a0c59
LucasBruce <lucasmatheusbruce@gmail.com>
2025-09-29 21:55:36 -0300

    desenv - Criando Layout inicial parte.1

--- ui-web/task-ui-web/src/app/app.ts
+++ ui-web/task-ui-web/src/app/app.ts
@@ -1,11 +1,10 @@
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { Component, signal, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -29,18 +28,39 @@ import { DomSanitizer } from '@angular/platform-browser';
         position="start"
       >
         <mat-nav-list class="m-3">
-          <a mat-list-item routerLink="/" (click)="drawer.toggle()">Mínimizar</a>
-          <a mat-list-item routerLink="/" (click)="drawer.toggle()">Temas</a>
-          <a mat-list-item routerLink="/" (click)="drawer.toggle()">Iniciar Aplicação com Segurança</a>
-          <a mat-list-item routerLink="/" (click)="drawer.toggle()">Iniciar Aplicação sem Segurança</a>
+          <a mat-list-item routerLink="/" (click)="toggle()">
+            Mínimizar
+            @if (this.isAngle) {
+              <i class="pi pi-angle-double-right"></i>
+            } @else {
+              <i class="pi pi-angle-double-left"></i>
+            }
+          </a>
+          <a mat-list-item routerLink="/" (click)="toggleSun()">
+            @if (this.isSun) {
+              {{ sunLabel }}
+              <i class="pi pi-sun"></i>
+            } @else {
+              {{ sunLabel }}
+              <i class="pi pi-moon"></i>
+            }
+          </a>
+          <a mat-list-item routerLink="/" (click)="toggleLock()">
+            Segurança
+            @if (this.isLock) {
+              <i class="pi pi-lock"></i>
+            } @else {
+              <i class="pi pi-lock-open"></i>
+            }
+          </a>
         </mat-nav-list>
       </mat-sidenav>
       <mat-sidenav-content>
         <mat-toolbar color="primary" class="mat-elevation-z4">
-          <button mat-icon-button (click)="drawer.toggle()" class="hide-desktop">
+          <button mat-icon-button (click)="toggle()" class="hide-desktop">
             <mat-icon>menu</mat-icon>
           </button>
-         
+
           <span style="margin-left: 8px;">Layout UI Web Task Manager</span>
         </mat-toolbar>
         <div style="padding: 24px;">
@@ -51,42 +71,29 @@ import { DomSanitizer } from '@angular/platform-browser';
   `,
   styleUrl: './app.scss',
 })
-export class App implements OnInit {
+export class App {
+  sunLabel = 'Light Mode';
+  isAngle = true;
+  isLock = true;
+  isSun = true;
+
   protected readonly title = signal('task-ui-web');
 
-  matIconRegistry = inject(MatIconRegistry);
+  @ViewChild('drawer') drawer!: MatSidenav;
 
-  domSanitizer = inject(DomSanitizer);
+  toggle() {
+    this.isAngle = !this.isAngle;
 
-  ngOnInit(): void {
-    this.matIconRegistry.addSvgIcon(
-      'home',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/home.svg')
-    );
-  
-    this.matIconRegistry.addSvgIcon(
-      'dark_mode',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/dark_mode.svg')
-    );
+    this.drawer.toggle();
+  }
 
-    this.matIconRegistry.addSvgIcon(
-      'light_mode',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/light_mode.svg')
-    );
+  toggleSun() {
+    this.isSun = !this.isSun;
 
-    this.matIconRegistry.addSvgIcon(
-      'minimize',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/minimize.svg')
-    );
+    this.sunLabel = this.isSun ? 'Light Mode' : 'Dark Mode';
+  }
 
-    this.matIconRegistry.addSvgIcon(
-      'encrypted_off',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/encrypted_off.svg')
-    );
-
-    this.matIconRegistry.addSvgIcon(
-      'encrypted_on',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/encrypted_on.svg')
-    );
+  toggleLock() {
+    this.isLock = !this.isLock;
   }
 }
